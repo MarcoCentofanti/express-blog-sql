317b74dcd932e00087815a172c7730e49d81facd
marcocentofanti <marcocentofanti@hotmail.it>
2026-05-07 17:05:12 +0200

    show function

--- controllers/controllerPosts.js
+++ controllers/controllerPosts.js
@@ -1,11 +1,25 @@
-const { post } = require("../routers/routerPosts");
+const connection = require("./../data/db");
 
 function index(req, res) {
-  res.send("index");
+  const sql = "SELECT * FROM posts";
+
+  connection.query(sql, (err, result) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json(result);
+  });
 }
 
 function show(req, res) {
-  res.send("Show");
+  const id = req.params.id;
+  const sql = `SELECT * FROM posts WHERE id = ?`;
+
+  connection.query(sql, [id], (err, postResult) => {
+    if (err) return res.status(500).json({ error: `Database query failed` });
+    if (postResult.lenght === 0)
+      return res.status(404).json({ error: `Post not found` });
+    const post = postResult[0];
+    res.json(post);
+  });
 }
 
 function create(req, res) {
